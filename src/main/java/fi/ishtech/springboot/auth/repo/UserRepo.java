aad288aa5e2b8d844006b17172f1e70194bc773f
Muneer Ahmed Syed <muneer@ishtech.fi>
2024-03-03 22:51:02 +0200

    entity and repo for User db table

---
+++ src/main/java/fi/ishtech/springboot/auth/repo/UserRepo.java
@@ -0,0 +1,30 @@
+package fi.ishtech.springboot.auth.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+
+import fi.ishtech.springboot.auth.entity.User;
+
+/**
+ * {@link Repository} for entity {@link User}
+ *
+ * @author Muneer Ahmed Syed
+ */
+public interface UserRepo extends JpaRepository<User, Long> {
+
+	boolean existsByUsername(String username);
+
+	Optional<User> findOneByUsername(String username);
+
+	@Query("SELECT u.passwordHash FROM User u WHERE u.id = :userId")
+	String findPasswordHashById(Long userId);
+
+	@Modifying
+	@Query("UPDATE User u SET u.passwordHash = :newPassword WHERE u.id = :userId")
+	void updatePassword(Long userId, String newPassword);
+
+}
