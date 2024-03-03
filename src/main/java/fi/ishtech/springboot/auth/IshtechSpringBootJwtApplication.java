bd63818e9559528548480b3af4778bd49e199c5d
Muneer Ahmed Syed <muneer@ishtech.fi>
2024-03-03 23:24:08 +0200

    move main class to package root

--- src/main/java/fi/ishtech/springboot/auth/IshtechSpringBootJwtApplication.java
+++ src/main/java/fi/ishtech/springboot/auth/IshtechSpringBootJwtApplication.java
@@ -1,4 +1,4 @@
-package fi.ishtech.springboot.ishtechspringbootjwt;
+package fi.ishtech.springboot.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
