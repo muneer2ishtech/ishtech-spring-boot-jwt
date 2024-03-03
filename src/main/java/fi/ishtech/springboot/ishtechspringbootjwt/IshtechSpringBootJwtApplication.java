ef4b07791abb048ba354791e068dfb81bf3270b0
Muneer Ahmed Syed <muneer@ishtech.fi>
2024-03-03 15:45:05 +0200

    Initial commit

---
+++ src/main/java/fi/ishtech/springboot/ishtechspringbootjwt/IshtechSpringBootJwtApplication.java
@@ -0,0 +1,13 @@
+package fi.ishtech.springboot.ishtechspringbootjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshtechSpringBootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshtechSpringBootJwtApplication.class, args);
+	}
+
+}
